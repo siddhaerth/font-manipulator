21c6244f90b96ba2a71b35499bcbd2280c949176
siddhaerth <83398730+siddhaerth@users.noreply.github.com>
2022-05-20 11:33:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-function setup(){
-    video=createCapture(VIDEO);
-    video.size(550,500);
 
-    canvas = createCanvas(500,450);
-    canvas.position(560,90);
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(500, 450);
+    canvas.position(560, 90);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
-function modelLoaded(){
+
+function modelLoaded() {
     console.log('PoseNet Is Initialized!')
 }
 
-function draw(){
-    background('#00FFFF');
+function draw() {
+    background('##00FF00');
+    document.getElementById("font_size").innerHTML = "Font size of text will be = "  + difference +"px";
+
+    fill('#008000');
+    
+    textSize(difference);
+    text('Siddhaerth', 50, 400);
 }
-function gotPoses(results){
-    if(results.length>0)
-    {
+
+function gotPoses(results) {
+    if (results.length > 0) {
         console.log(results);
+
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX =" + leftWristX + "rightWristX" + rightWristX + "difference =" + difference);
     }
 }
